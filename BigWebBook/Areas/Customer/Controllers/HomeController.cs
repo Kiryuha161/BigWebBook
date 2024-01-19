48923beb35c090dbc6be9a47028d31c7e77a8e7c
Kiryuha161 <108215760+Kiryuha161@users.noreply.github.com>
2024-01-20 01:48:12 +0300

    Вёрстка главной страницы - отображение карточек созданных в админке книг

--- BigWebBook/Areas/Customer/Controllers/HomeController.cs
+++ BigWebBook/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using BigWeb.Models;
+﻿using BigWeb.DataAccess.Data;
+using BigWeb.DataAccess.Repository.IRepository;
+using BigWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -8,15 +10,18 @@ namespace BigWebBook.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> products = _unitOfWork.Product.GetAll(includeProperties: "CategoryName");
+            return View(products);
         }
 
         public IActionResult Privacy()
